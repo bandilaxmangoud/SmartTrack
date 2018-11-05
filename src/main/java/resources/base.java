aa2d9f6bc5949216ac6c2825fda60f9f0691caeb
bandilaxmangoud <bandi.laxmangoud@gmail.com>
2018-11-05 22:46:13 +0530

    main

--- src/main/java/resources/base.java
+++ src/main/java/resources/base.java
@@ -22,21 +22,21 @@ public class base {
 	public WebDriver initializeDriver() throws IOException {
 		prop = new Properties();
 		FileInputStream fis = new FileInputStream(
-				"D:\\WORK\\CommonFramework\\src\\main\\java\\resources\\data.properties");
+				"drivers//data.properties");
 
 		prop.load(fis);
 		String browserName = prop.getProperty("browser");
 
 		if (browserName.equals("chrome")) {
-			System.setProperty("webdriver.chrome.driver", "D:\\SOFTWARES\\Jars\\chromedriver_win32\\chromedriver.exe");
+			System.setProperty("webdriver.chrome.driver", "drivers//chromedriver.exe");
 			driver = new ChromeDriver();
 		} else if (browserName.equals("firefox")) {
 			System.setProperty("webdriver.gecko.driver",
-					"D:\\SOFTWARES\\Jars\\geckodriver-v0.21.0-win64\\geckodriver.exe");
+					"drivers//geckodriver.exe");
 			driver = new FirefoxDriver();
 		} else if (browserName.equals("ie")) {
 			System.setProperty("webdriver.ie.driver",
-					"D:\\SOFTWARES\\Jars\\IEDriverServer_x64_3.14.0\\IEDriverServer.exe");
+					"drivers//IEDriverServer.exe");
 			driver = new InternetExplorerDriver();
 		}
 
